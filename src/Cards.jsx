f72f470f46e2a68360a4351e09a3338d58c8633c
Gaurav <gauravkrdas19@gmail.com>
2025-03-13 18:55:06 +0530

    card content popup

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { TbFileSmile } from "react-icons/tb";
+import React, { useState } from 'react'
+import { TbFileSmile} from "react-icons/tb";
 import { motion } from "motion/react"
+import { RxCross2 } from "react-icons/rx";
 
 
 function Cards({cardDetails,foregroundRef}) {
+  var [view,setView] = useState(false);
   return (
-    <motion.div drag 
+    (!view)?<motion.div drag 
     dragConstraints={foregroundRef}
     whileDrag={{scale:1.1, cursor:"grabbing"}}
     dragTransition={{bounceStiffness:600, bounceDamping:25,}}
-    className='text-zinc-300  w-60 h-75 bg-zinc-600 rounded-2xl cursor-grab overflow-clip'>
+    className='relative text-zinc-300  w-60 h-75 bg-zinc-600 rounded-2xl cursor-grab overflow-clip'>
         <div className='flex px-5 pt-6'>
             <TbFileSmile className='text-4xl mr-1'/>
             <div>
@@ -17,12 +19,43 @@ function Cards({cardDetails,foregroundRef}) {
                 <p className='leading-2 text-zinc-400 text-sm'>{cardDetails.meta}</p>
             </div>
         </div>
-        <p className='mt-6 px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dignissimos blanditiis corporis recusandae laudantium ut!</p>
-        <div className='bg-orange-400 h-15 mt-10'>
-          <p className='flex justify-center items-center h-full cursor-pointer text-2xl font-semibold'>VIEW NOTE</p>
+        <p className='mt-6 px-5'>{cardDetails.desc}</p>
+        <div className='absolute bg-orange-400 h-15 bottom-0 w-full'>
+          <p className='flex justify-center items-center h-full cursor-pointer text-2xl font-semibold'
+          onClick={()=>{
+            setView(true);
+            viewCardDesc(cardDetails);
+          }
+          }
+          >VIEW NOTE</p>
         </div>
     </motion.div>
+    :
+    <div className=' w-full h-screen absolute top-0 backdrop-blur-md z-20'>
+      <div className='absolute text-zinc-300  w-1/2 h-3/4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-zinc-600 rounded-2xl overflow-clip p-4 z-50 shadow-[0_0_6px_rgba(0,0,0,0.25)]'>
+        <div className='flex justify-between'>
+          <div className='flex'>
+          <TbFileSmile className='text-6xl mr-1'/>
+            <div>
+              <p className='text-4xl'>{
+                cardDetails.title
+              }</p>
+              <p className='leading-2 text-zinc-400'>{
+                cardDetails.meta
+              }</p>
+            </div>
+          </div>
+          <RxCross2 className='text-zinc-300 text-4xl hover:text-zinc-400 cursor-pointer hover:scale-[1.1]' onClick={()=>setView(false)}/>
+        </div>
+        <p className='mt-10 px-4'>{cardDetails.desc}</p>
+      </div>
+    </div>
   )
+}
+
+function viewCardDesc(cardDetails){
+  setView(true);
+  console.log(cardDetails.desc);
 }
 
 export default Cards
--- src/Foreground.jsx
+++ src/Foreground.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react'
 import Cards from './Cards';
+import { FiPlusCircle } from "react-icons/fi";
+import { Tooltip } from 'react-tooltip'
 
 function Foreground() {
     const componentRef = useRef(null);
@@ -14,7 +16,12 @@ function Foreground() {
         },
         {
         "title": "File 2",
-        "desc": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe consectetur dicta vitae doloremque laborum earum!",
+        "desc": "Hello",
+        "meta": d.getDate()+" "+months[d.getMonth()]+", "+d.getFullYear()
+        },
+        {
+        "title": "File 3",
+        "desc": "sx",
         "meta": d.getDate()+" "+months[d.getMonth()]+", "+d.getFullYear()
         },
     ]
@@ -23,9 +30,18 @@ function Foreground() {
     <div ref={componentRef} className='overflow-hidden fixed flex flex-wrap gap-2 top-0 left-0 w-full h-screen p-4 z-10'>
         {
             cardInfo.map((item,index)=>(
-                <Cards cardDetails={item} foregroundRef={componentRef}/>
+                (item.desc!=="")?<Cards key={index} cardDetails={item} foregroundRef={componentRef}/>: null
             ))
         }
+        <div className='absolute top-8 right-8'>
+            <FiPlusCircle data-tooltip-id='addNew' className='text-zinc-500 text-5xl cursor-pointer'/>
+            <Tooltip
+                id='addNew'
+                content='Add new'
+                place='bottom'
+                variant='light'
+            />
+        </div>
     </div>
   );
 }
