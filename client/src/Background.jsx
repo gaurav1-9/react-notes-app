4d488e5502c70ac6b4fa1c950f8105c8b4077d2b
Gaurav <gauravkrdas19@gmail.com>
2025-04-01 20:11:56 +0530

    bg responsiveness mobile only

--- client/src/Background.jsx
+++ client/src/Background.jsx
@@ -4,8 +4,8 @@ function Background() {
   return (
     <>
       <div className='fixed w-full h-screen bg-zinc-800 z-0'>
-        <h3 className='text-zinc-300/50 flex justify-center items-center p-20 text-md lg:text-xl '>NOTES LIST</h3>
-        <h1 className="text-zinc-700/30 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[100px] sm:text-[80px] md:text-[150px] lg:text-[200px]">notes</h1>
+        <h3 className='text-zinc-300/50 flex justify-center items-center pt-10 lg:p-20 text-md lg:text-xl'>NOTES LIST</h3>
+        <h1 className="text-zinc-700/30 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[100px] lg:text-[200px]">notes</h1>
       </div>
     </>
   )
